3c34acb24ee8808c82c0f673fd0c2502ac4efb9c
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2021-03-31 08:15:18 +0200

    wzburzający update
    Added:
    -duck now have RB
    -Splash and TouchSpawn(needs to include touch)classes
    -Thanks for Duckttention

---
+++ DuckingBread/Assets/Scripts/Splash.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    public float power = 3.0f;
+    public float radius = 5.0f;
+    public float upforce = 1.0f;
+    public int destroyTime = 1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Detonate();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Detonate();
+        Destroy(gameObject, destroyTime);
+
+
+    }
+    void Detonate()
+    {
+        Vector3 explosionPosition = this.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach(Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if(rb!=null)
+            {
+              
+                rb.AddExplosionForce(power, explosionPosition, radius, upforce, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
---
+++ DuckingBread/Assets/Scripts/Touch/TouchSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TouchSpawn : MonoBehaviour
+{
+    public GameObject prefab;
+    public Camera MainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                Instantiate(prefab, new Vector3(hit.point.x, hit.point.y +prefab.transform.position.y, hit.point.z), Quaternion.identity);
+            }
+           
+        }
+    }
+}
