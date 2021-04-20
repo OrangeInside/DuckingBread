c28d9afa51592f31de64f3849df9b175f0ca80ab
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2021-04-20 10:49:49 +0200

    GameController added
    VERY SIMPLE ONE

---
+++ DuckingBread/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int lives = 3;
+    public GameObject[] ducks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ducks = GameObject.FindGameObjectsWithTag("Duck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
