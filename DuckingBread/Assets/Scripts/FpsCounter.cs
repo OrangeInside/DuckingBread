5ae788de32dc7e9310650a034554a16580b561ef
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-04-27 13:48:23 +0200

    Fps counter code, font, position fix

--- DuckingBread/Assets/Scripts/FpsCounter.cs
+++ DuckingBread/Assets/Scripts/FpsCounter.cs
@@ -2,31 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FpsCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float timer, refresh, avgFramerate;
+    private TextMeshProUGUI fpsText;
 
-   
+    private void Start()
+    {
+        fpsText = GetComponent<TextMeshProUGUI>();
+    }
 
-        public float timer, refresh, avgFramerate;
-        string display = "{0} FPS";
-    public Text m_Text;
+    private void Update()
+    {
+        //Change smoothDeltaTime to deltaTime or fixedDeltaTime to see the difference
+        float timelapse = Time.deltaTime;
+        timer = timer <= 0 ? refresh : timer -= timelapse;
 
-        private void Start()
-        {
-            m_Text = GetComponent<Text>();
-        }
+        if (timer <= 0) 
+            avgFramerate = (int)(1f / timelapse);
 
-
-        private void Update()
-        {
-            //Change smoothDeltaTime to deltaTime or fixedDeltaTime to see the difference
-            float timelapse = Time.deltaTime;
-            timer = timer <= 0 ? refresh : timer -= timelapse;
-
-            if (timer <= 0) avgFramerate = (int)(1f / timelapse);
-            m_Text.text = string.Format(display, avgFramerate.ToString());
-        }
+        fpsText.text = avgFramerate.ToString();
+    }
     
 }
