ba9f97b2b56c76f2a895afa84fe5e5b50e66a699
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2021-04-27 10:49:09 +0200

    fps counter + vsync off

---
+++ DuckingBread/Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   
+
+        public float timer, refresh, avgFramerate;
+        string display = "{0} FPS";
+    public Text m_Text;
+
+        private void Start()
+        {
+            m_Text = GetComponent<Text>();
+        }
+
+
+        private void Update()
+        {
+            //Change smoothDeltaTime to deltaTime or fixedDeltaTime to see the difference
+            float timelapse = Time.deltaTime;
+            timer = timer <= 0 ? refresh : timer -= timelapse;
+
+            if (timer <= 0) avgFramerate = (int)(1f / timelapse);
+            m_Text.text = string.Format(display, avgFramerate.ToString());
+        }
+    
+}
