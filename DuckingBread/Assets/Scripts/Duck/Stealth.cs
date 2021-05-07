c9f92ccbd1fca45447644e579b62a4e3b142f922
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2021-05-07 18:21:35 +0200

    stealth transparency  range added

--- DuckingBread/Assets/Scripts/Duck/Stealth.cs
+++ DuckingBread/Assets/Scripts/Duck/Stealth.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Stealth : MonoBehaviour
 {
    public  GameObject duck;
+    [Range(0,1)]
     public float trans = 0.1f;
     // Start is called before the first frame update
     void Start()
