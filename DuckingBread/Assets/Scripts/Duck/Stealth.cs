2cd38e7778f045e2c1e4472c832a5dada15f4376
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2021-05-07 15:04:10 +0200

    stealth mechanic added but need some fixes

---
+++ DuckingBread/Assets/Scripts/Duck/Stealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stealth : MonoBehaviour
+{
+   public  GameObject duck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter(Collider coll )
+    {
+     
+        if (coll.gameObject.tag == "Grass")
+        {
+            Color color = duck.GetComponent <SkinnedMeshRenderer> ().material.color;
+            color.a =0.4f;
+            duck.GetComponent<SkinnedMeshRenderer>().material.color = color;
+            Debug.Log("Znikam");
+        }
+      
+    }
+     void OnTriggerExit( )
+    {
+       
+            Color color = duck.GetComponent<SkinnedMeshRenderer>().material.color;
+            color.a = 1f;
+            duck.GetComponent<SkinnedMeshRenderer>().material.color = color;
+       
+
+    }
+}
